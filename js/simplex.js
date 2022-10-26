5631425849c3b18d477211053044a69291aab43e
Geraldo Junior <geraldojunioroficial@gmail.com>
2022-10-26 20:35:35 -0300

    Alteração da análise de sensibilidade

--- js/simplex.js
+++ js/simplex.js
@@ -2,7 +2,6 @@ var loops = 1;
 var MAX_LOOPS;
 var STOPBYLOOP = MAX_LOOPS + 1;
 
-//var FUNCAO = "-3;-2";
 var FUNCAO = [];
 
 function simplex() {
@@ -208,51 +207,6 @@ function solucao() {
         "<div class='ui list'>" + header + paragrafos + "</div>";
 }
 
-/*
-function printTablePreco() {
-    var results = document.getElementById("tablePreco");
-    var htmlTitle = '<h3 class="text-center">Tabela otimizada</h3>';
-    var table = '<table class="ui table">';
-    var header = "<thead><tr>";
-
-    var cabecalho = ["Linha", "V.F.", "Preco Soma", "Aumenta", "Reduz"];
-    for (var col = 0; col < cabecalho.length; col++)
-        header += "<th>" + cabecalho[col] + "</th>";
-    table += header + "</tr></thead>";
-
-    var coluna1 = ["X1", "X2", "S1", "S2", "S3", "ZW"];
-
-    // Pega valores
-    var z = matrix[matrix.length - 1][matrix[matrix.length - 1].length - 1];
-
-    for (var i = 1; i < matrix[0].length - 1; i++) {
-        var solucao =
-            (matrix[0][i][0] == "x" ? "Produção de " : "Folga da restrição ") +
-            matrix[0][i];
-    }
-
-    console.log(solucao);
-
-    var body = "<tbody>";
-    for (var col = 1; col < coluna1.length; col++) {
-        var tr = "<tr>";
-        //for (var col = 0; col < matrix[row].length; col++){
-        for (var row = 0; row < 2; row++) {
-            if (col == 0) {
-                tr += "<td>" + cabecalho[row] + "</td>";
-            } else {
-                console.log("row: ", row, "col: ", col)
-                tr += "<td>" + matrix[row][col] + "</td>";
-            }
-        }
-
-        body += tr + "</tr>";
-    }
-
-    table += body + "</tbody>";
-    results.innerHTML += htmlTitle + table + "</table><hr />";
-}*/
-
 function sensibilidade() {
 
     var outputDiv = document.getElementById('sensibility-analysis');
@@ -260,7 +214,7 @@ function sensibilidade() {
     var paragraphs = '';
     var paragraphsX = '';
     var subjects = document.querySelectorAll('#subjects .restrictionV');
-    
+
     var subjectIndex = (matrix[0].length - subjects.length) - 1;
     for (var index = 0; index < subjects.length; index++, subjectIndex++) {
         var restricao = 'f' + (index + 1);
@@ -270,7 +224,7 @@ function sensibilidade() {
 
         var shadowPrice = matrix[matrix.length - 1][subjectIndex];
 
-        //if (shadowPrice != 0) {
+        if (shadowPrice != 0) {
             for (var rowIndex = 1; rowIndex < (matrix.length - 1); rowIndex++) {
                 var functionRow = Number(matrix[rowIndex][subjectIndex]);
                 var bRow = Number(matrix[rowIndex][matrix[0].length - 1]);
@@ -280,35 +234,32 @@ function sensibilidade() {
 
                 var delta = (-1 * bRow) / functionRow;
 
-                if (delta > maxDelta)
+                if (delta > maxDelta) {
                     maxDelta = delta;
+                }
 
-                if (delta < minDelta)
+                if (delta < minDelta) {
                     minDelta = delta;
+                }
             }
-            minDelta += original;
-            console.log("minDelta: ",minDelta)
-            maxDelta += original;
-            console.log("maxDelta: ",maxDelta)
+
+            minDelta = Math.abs(minDelta);
             paragraphs += '<tr><td>' + restricao + '</td><td>' + original + '</td><td>' + shadowPrice.toFixed(2) + '</td><td>' + maxDelta.toFixed(2) + '</td><td>' + minDelta.toFixed(2) + '</td></tr>';
-        // } else {
-        //     paragraphs += '<tr><td>' + restricao + '</td><td>' + original + '</td><td>0</td><td class="inf">∞</td><td class="inf">∞</td></tr>';
-        // }
+        } else {
+            paragraphs += '<tr><td>' + restricao + '</td><td>' + original + '</td><td>0</td><td class="inf">∞</td><td class="inf">∞</td></tr>';
+        }
     }
-    
+
     var z = FUNCAO;
-    console.table(matrix);
-    
+
     jQuery.each(z, function (index, value) {
         var decision = 'x' + (index + 1);
-        var originalX = value*-1;
-        console.log("decision: ",decision," | originalX: ",originalX)
+        var originalX = value * -1;
         var minDeltaX = Number.POSITIVE_INFINITY;
         var maxDeltaX = Number.NEGATIVE_INFINITY;
         var shadowPriceX = matrix[matrix.length - 1][index + 1];
-        
-        console.log("shadowPriceX: ",shadowPriceX)
-        if (shadowPriceX != 0) {
+
+        if (shadowPriceX == 1) {
             for (var i = 1; i < (matrix.length - 1); i++) {
                 var functionRow = Number(matrix[i][index + 1]);
                 var b = Number(matrix[i][matrix[0].length - 1]);
@@ -316,13 +267,7 @@ function sensibilidade() {
                 if (functionRow == 0)
                     continue;
 
-                var deltaX = (-1 * b) / functionRow;
-                console.log("deltaX: ",deltaX)
-                console.log("deltaX: ",deltaX)
-                
-                console.log("b: ",b)
-                console.log("functionRow: ",functionRow)
-                console.log("deltaX*1: ", (b) / functionRow)
+                var deltaX = (1 * b) / functionRow;
 
                 if (deltaX > maxDeltaX)
                     maxDeltaX = deltaX;
@@ -331,16 +276,16 @@ function sensibilidade() {
                     minDeltaX = deltaX;
             }
             minDeltaX += parseFloat(originalX);
-            console.log("minDeltaX: ",minDeltaX)
+
             maxDeltaX += parseFloat(originalX);
-            console.log("maxDeltaX: ",maxDeltaX)
+
             paragraphsX += '<tr><td>' + decision + '</td><td>' + originalX + '</td><td>' + shadowPriceX.toFixed(2) + '</td><td>' + maxDeltaX.toFixed(2) + '</td><td>' + minDeltaX.toFixed(2) + '</td></tr>';
         } else {
             paragraphsX += '<tr><td>' + decision + '</td><td>' + originalX + '</td><td>-</td><td>-</td><td>-</td></tr>';
         }
     });
 
-    
+
     var z_value = matrix[matrix.length - 1][matrix[matrix.length - 1].length - 1];
     paragraphs += '<tr><td> Z </td><td>' + z_value.toFixed(2) + '</td><td>-</td><td>-</td><td>-</td></tr>';
 
